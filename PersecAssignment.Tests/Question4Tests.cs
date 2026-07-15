5cca27fe126f9078439c93a65a977ccb6e23c44d
Pachara Nokroy <pacharanokroy2003@gmail.com>
2026-07-15 15:37:09 +0700

    feat: add Question4 class with integer to Roman numeral conversion and add unit tests

---
+++ PersecAssignment.Tests/Question4Tests.cs
@@ -0,0 +1,16 @@
+namespace PersecAssignment.Tests
+{
+    public class Question4Tests
+    {
+        [Fact]
+        public void IntToRoman_ShouldConvertCorrectly()
+        {
+            var question4 = new Question4();
+
+            Assert.Equal("MCMLXXXIX", question4.IntToRoman(1989));
+            Assert.Equal("MM", question4.IntToRoman(2000));
+            Assert.Equal("LXVIII", question4.IntToRoman(68));
+            Assert.Equal("CIX", question4.IntToRoman(109));
+        }
+    }
+}
---
+++ PersecAssignment/Question4.cs
@@ -0,0 +1,31 @@
+namespace PersecAssignment
+{
+    public class Question4
+    {
+        public string IntToRoman(int num) 
+        {
+            string romanResult = "";
+
+            Dictionary<string, int> romanNumbersDictionary = new() 
+            {
+                { "I", 1 }, { "IV", 4 }, { "V", 5 }, { "IX", 9 }, 
+                { "X", 10 }, { "XL", 40 }, { "L", 50 }, { "XC", 90 }, 
+                { "C", 100 }, { "CD", 400 }, { "D", 500 }, { "CM", 900 }, 
+                { "M", 1000 }
+            };
+
+            foreach(var item in romanNumbersDictionary.Reverse()) 
+            {
+                if (num <= 0) break;
+                
+                while (num >= item.Value) 
+                {
+                    romanResult += item.Key;
+                    num -= item.Value;
+                }
+            }
+            
+            return romanResult;
+        }
+    }
+}
